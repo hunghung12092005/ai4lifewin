18383188a16d0c574252deb988bb77041c30f64a
hunghung12092005 <hunghung12092005@gmail.com>
2025-08-09 10:16:06 +0700

    code

--- laravel-project/routes/web.php
+++ laravel-project/routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\aiController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/chat', [aiController::class, 'hotelLinks']);
