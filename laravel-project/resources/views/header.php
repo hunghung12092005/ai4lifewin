d2e479fed3934b31d67f643f48f0898b025e731f
Thọ Lê Minh <tho@example.com>
2025-08-09 10:06:13 +0700

    push lan 1

---
+++ laravel-project/resources/views/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edrio - Online Courses</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+</head>
+<body>
+    <header class="main-header">
+        <div class="container">
+            <nav class="main-nav">
+                <a href="index.php" class="logo">Edrio.</a>
+                <ul class="nav-menu">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Course <i class="fa-solid fa-chevron-down"></i></a></li>
+                    <li><a href="#">Pages <i class="fa-solid fa-chevron-down"></i></a></li>
+                    <li><a href="#">Blog <i class="fa-solid fa-chevron-down"></i></a></li>
+                </ul>
+                <div class="nav-right">
+                    <a href="#" class="cart-icon"><i class="fa-solid fa-cart-shopping"></i></a>
+                    <a href="#" class="btn btn-primary">Get Quote Now</a>
+                </div>
+            </nav>
+        </div>
+    </header>
+<main>
